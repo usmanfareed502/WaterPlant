ac7310985e28b0282fc1ac7a030109f50fddc666
Far45han <134602710+Far45han@users.noreply.github.com>
2023-10-09 15:34:26 +0500

    new

--- src/app/pages/dataenter/dataenter.component.ts
+++ src/app/pages/dataenter/dataenter.component.ts
@@ -8,30 +8,28 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./dataenter.component.scss']
 })
 export class DataenterComponent {
-  
-  public alldata: any = {
-    Customer: 'Customer', Expence: 'Expence', Add: 'Add', Customer_Name: 'Customer Name', Customer_Number: 'Customer Number',
-    Customer_Address: 'Customer Address', close: 'close', Expance: 'Expance', Name: 'Name', Descripition: 'Descripition',
-    Date: 'Date', Amount: 'Amount', BILL: 'BILL', Details: 'Details',Addcustomer:'Add Customer',
+
+  public stdata: any = {
+    Customer_Name: 'Customer Name', Customer_Number: 'Customer Number',
+    Customer_Address: 'Customer Address', close: 'close', Addcustomer: 'Add Customer',
+    date: 'Date', security: 'Security', No_of_bottels: 'No of bottels', return: 'Return', empty: 'Empty', amount: 'Amount',
+    descount: 'Descount in %', total: 'Total', sucmitted: 'Sucmitted bill', remeaning: 'Remeaning bill', submit: 'Submit'
   }
-  public data: any = {
-    Customer_Name: 'Customer Name',Customer_Number:'Customer Number',Customer_Address: 'Customer Address',submit: 'Submit',
-    date:'Date',security:'Security',No_of_bottels:'No of bottels',return:'Return',empty:'Empty',amount:'Amount',
-    descount:'Descount in %',total:'Total',sucmitted:'Sucmitted bill',remeaning:'Remeaning bill'
+
+  public insertbill: any = {
+    customer: '',
+    date: '', security: '', No_of_bottels: '', return: '', empty: '', amount: '',
+    descount: '', total: '', sucmitted: '', remeaning: ''
   }
- 
-  // public addmorebtn: string = 'cancel';
-  // public linkcolordropdownSettings={}
-  // public allowSearchFilter=true;
-  // public allprintcolor: any = [];
-  // public user: any = { name: '', father_name: ''}
+
+
   public user: any = { name: '', number: '', address: '' }
   public data1: any = { name: '', number: '', address: '' }
   public getcusromer1: any;
- 
+
   constructor(public apicall: ApicallService, public global: GlobalService) { }
 
-async  ngOnInit() {
+  async ngOnInit() {
     await this.apicall.api_getcustomer();
     this.global.Getcustomer.subscribe(res => {
       this.getcusromer1 = res;
@@ -40,11 +38,11 @@ async  ngOnInit() {
   }
 
   async addcustomer() {
- 
+
     await this.apicall.api_addcustomer(this.user)
     console.log(this.user)
     this.user = { name: '', number: '', address: '' }
   }
 
-   
+
 }
